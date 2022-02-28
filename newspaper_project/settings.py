945d89f8c9ace7ae68f5bd543863395db1c729d0
lutusyo <boni>
2022-03-01 02:01:00 +0300

    password change and reset

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
